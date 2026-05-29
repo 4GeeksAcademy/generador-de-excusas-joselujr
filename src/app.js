08c1076ce0c5650342145fcedf45fc6715430d73
joselujr <masullo.joseluis@gmail.com>
2026-05-29 21:01:52 +0000

    he terminado el generador

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //             0          1               2              3         4
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  let excuse = document.querySelector("#excuse")
+  console.log(Math.floor(Math.random() * 4));
+  console.log(who[Math.floor(Math.random() * 4)] + " " + action[Math.floor(Math.random() * 4)] + " " + what[Math.floor(Math.random() * 3)] + " " + when[Math.floor(Math.random() * 5)])
+  excuse.innerHTML = who[Math.floor(Math.random() * 4)] + " " + action[Math.floor(Math.random() * 4)] + " " + what[Math.floor(Math.random() * 3)] + " " + when[Math.floor(Math.random() * 5)]
+  }
